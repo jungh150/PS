ab8524695d6b0ca008cd864bc617810247e23dc7
jungh150 <jungh150@naver.com>
2023-09-26 01:09:33 +0900

    20230925

---
+++ baekjoon/1764.py
@@ -0,0 +1,36 @@
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+
+notHeard = []
+notSeen = []
+ans = []
+
+for _ in range(0, n):
+    notHeard.append(stdin.readline().strip())
+
+for _ in range(0, m):
+    notSeen.append(stdin.readline().strip())
+
+notHeard = sorted(notHeard)
+notSeen = sorted(notSeen)
+hIndex = 0
+sIndex = 0
+
+print(notHeard)
+print(notSeen)
+
+while (hIndex < n) & (sIndex < m):
+    if (notHeard[hIndex] == notSeen[sIndex]):
+        ans.append(notHeard[hIndex])
+        hIndex += 1
+        sIndex += 1
+    else:
+        if (notHeard[hIndex] > notSeen[sIndex]):
+            sIndex += 1
+        else:
+            hIndex += 1
+
+print(len(ans))
+for i in range(0, len(ans)):
+    print(ans[i])
