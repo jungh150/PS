770e4d048649b7bd6b53d58cd92c6472187c83a0
jungh150 <jungh150@naver.com>
2024-02-13 18:47:30 +0900

    20240211

---
+++ baekjoon/15810.py
@@ -0,0 +1,20 @@
+from sys import stdin
+from queue import PriorityQueue
+
+n, m = map(int, stdin.readline().split())
+balloon = list(map(int, stdin.readline().split()))
+
+s = 1
+e = 10 ** 12 + 1
+
+while s <= e:
+    mid = (s + e) // 2
+    tmp = 0
+    for x in balloon:
+        tmp += mid // x
+    if tmp < m:
+        s = mid + 1
+    else:
+        e = mid - 1
+
+print(s)
