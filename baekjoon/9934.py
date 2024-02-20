cdda9f49c922a07850297b8c8d993eea6e9b9cb2
jungh150 <jungh150@naver.com>
2024-02-20 20:42:16 +0900

    20240220 (3)

--- baekjoon/9934.py
+++ baekjoon/9934.py
@@ -8,12 +8,23 @@ tree = {}
 for i in range(1, pow(2, k-1)):
     tree[i] = [i*2, i*2 + 1, 0]
 
-def inOrder(now, i):
-    if now != '.':
-        inOrder(tree[now][0], i)
-        tree[now][3] = visit[i]
-        print(tree[now][3])
-        i += 1
-        inOrder(tree[now][1], i)
+for i in range(pow(2, k-1), pow(2, k)):
+    tree[i] = ['.', '.', 0]
 
-inOrder(1, 0)
+def inOrder(now):
+    global i
+    if now != '.':
+        inOrder(tree[now][0])
+        tree[now][2] = visit[i]
+        i += 1
+        inOrder(tree[now][1])
+
+i = 0
+inOrder(1)
+
+level = 1
+for i in range(1, pow(2, k)):
+    if i == pow(2, level):
+        print()
+        level += 1
+    print(tree[i][2], end=' ')
