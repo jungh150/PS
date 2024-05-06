c1b03a2edce029c425c654244a63ba6134fefa90
jungh150 <jungh150@naver.com>
2024-05-07 01:31:35 +0900

    20240506 (2)

--- baekjoon/13398.py
+++ baekjoon/13398.py
@@ -10,7 +10,7 @@ res = l[0]
 
 for i in range(1, n):
     l[i] = max(data[i], l[i - 1] + data[i])
-    res = max(res, l[i]) # 수를 제거하지 않았을 때의 최솟값
+    res = max(res, l[i]) # 수를 제거하지 않았을 때의 최댓값
 
 r = [0 for _ in range(n)]
 r[n - 1] = data[n - 1]
@@ -19,7 +19,7 @@ for i in range(n - 2, -1, -1):
     r[i] = max(data[i], r[i + 1] + data[i])
 
 for i in range(1, n - 1):
-    tmp = l[i - 1] + r[i + 1]
+    tmp = l[i - 1] + r[i + 1] # 수를 제거했을 때의 최댓값
     res = max(res, tmp)
 
 print(res)
