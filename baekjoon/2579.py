f46b3995835531b442eb3d904a852500fbe86bad
jungh150 <jungh150@naver.com>
2024-05-09 23:46:39 +0900

    20240509

--- baekjoon/2579.py
+++ baekjoon/2579.py
@@ -1,17 +1,19 @@
-from sys import stdin
+import sys
+input = sys.stdin.readline
 
-n = int(stdin.readline())
-step = []
-dp = [0 for _ in range(n)]
+n = int(input())
+step = [0 for _ in range(n + 1)]
+dp = [0 for _ in range(n + 1)]
 
-for _ in range(n):
-    step.append(int(stdin.readline()))
+for i in range(1, n + 1):
+    step[i] = int(input())
 
-dp[0] = step[0]
-dp[1] = dp[0] + step[1]
-dp[2] = max()
+dp[1] = step[1]
 
-for i in range(3, n):
-    dp[i] = dp[i - 3] + max(step[i - 2], step[i - 1]) + step[i]
+if n >= 2:
+    dp[2] = dp[1] + step[2]
 
-print(dp)
+for i in range(3, n + 1):
+    dp[i] = max(dp[i - 2], dp[i - 3] + step[i - 1]) + step[i]
+
+print(dp[n])
