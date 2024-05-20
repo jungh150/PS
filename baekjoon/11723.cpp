8e5680e458973912053ae40a281158c6a67f9ed0
jungh150 <jungh150@naver.com>
2024-05-21 02:33:19 +0900

    20240521

---
+++ baekjoon/11723.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int m, n;
+    string op;
+    cin >> m;
+    int now = 0;
+
+    for (int i = 0; i < m; i++) {
+        cin >> op;
+        if (op == "add") {
+            cin >> n;
+            now = now | (1 << (n - 1));
+        }
+        else if (op == "remove") {
+            cin >> n;
+            if ((now & (1 << (n - 1))) != 0) {
+                now = now ^ (1 << (n - 1));
+            }
+        }
+        else if (op == "check") {
+            cin >> n;
+            if ((now & (1 << (n - 1))) == 0) {
+                cout << 0 << '\n';
+            }
+            else {
+                cout << 1 << '\n';
+            }
+        }
+        else if (op == "toggle") {
+            cin >> n;
+            now = now ^ (1 << (n - 1));
+        }
+        else if (op == "all") {
+            now = -1;
+        }
+        else if (op == "empty") {
+            now = 0;
+        }
+    }
+}
