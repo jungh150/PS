6ba276cb3edc3287922744edca239ddacafb429c
jungh150 <jungh150@naver.com>
2024-11-30 15:39:50 +0900

    20241130

---
+++ baekjoon/27940.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < m; i++) {
+        int t, r;
+        cin >> t >> r;
+        k -= r;
+        
+        if (k < 0) {
+            cout << i + 1 << ' ' << 1 << '\n';
+            return 0;
+        }
+    }
+
+    cout << -1 << '\n';
+}
