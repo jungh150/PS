86f89a492aa7fae6c7007dc99398a5e650026f81
Junghwa <jungh150@naver.com>
2025-01-05 01:25:55 +0900

    20250104

---
+++ baekjoon/11048.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int dx[3] = {1, 0, 1};
+int dy[3] = {0, 1, 1};
+vector<vector<int>> a;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    a = vector<vector<int>>(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 1; i < n; i++) a[i][0] += a[i - 1][0];
+    for (int j = 1; j < m; j++) a[0][j] += a[0][j - 1];
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            a[i][j] += max(a[i - 1][j], a[i][j - 1]);
+        }
+    }
+
+    cout << a[n - 1][m - 1] << '\n';
+}
