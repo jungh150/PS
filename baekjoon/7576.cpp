5cef7cc55f4ece71cc1181a836d702f8dfc4e45d
jungh150 <jungh150@naver.com>
2025-02-27 01:47:57 +0900

    20250226

--- baekjoon/7576.cpp
+++ baekjoon/7576.cpp
@@ -3,56 +3,37 @@
 #include <vector>
 using namespace std;
 
-int dn[4] = {0, 0, -1, 1};
-int dm[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
 vector<vector<int>> box;
+vector<vector<int>> ans;
 int m, n;
-int t = 0;
-
-bool check() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (box[i][j] == 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
 
 void bfs() {
-    queue<vector<int>> q;
+    queue<pair<int, int>> q;
     
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (box[i][j] == 1) {
-                q.push({i, j});
+                q.emplace(i, j);
+                ans[i][j] = 0;
             }
         }
     }
 
     while (!q.empty()) {
-        int tmp = q.size();
-        for (int i = 0; i < tmp; i++) {
-            vector<int> cur = q.front();
-            q.pop();
-            for (int x = 0; x < 4; x++) {
-                int ni = cur[0] + dn[x];
-                int nj = cur[1] + dm[x];
-                if (ni >= 0 && ni < n && nj >= 0 && nj < m && box[ni][nj] == 0) {
-                    q.push({ni, nj});
-                    box[ni][nj] = 1;
-                }
+        int curi = q.front().first;
+        int curj = q.front().second;
+        q.pop();
+        for (int k = 0; k < 4; k++) {
+            int ni = curi + dx[k];
+            int nj = curj + dy[k];
+            if (ni >= 0 && ni < n && nj >= 0 && nj < m && box[ni][nj] == 0) {
+                q.emplace(ni, nj);
+                box[ni][nj] = 1;
+                ans[ni][nj] = ans[curi][curj] + 1;
             }
         }
-        t++;
-        if (check()) {
-            break;
-        }
-    }
-
-    if (!check()) {
-        t = -1;
     }
 }
 
@@ -63,6 +44,7 @@ int main() {
 
     cin >> m >> n;
     box = vector<vector<int>>(n, vector<int>(m));
+    ans = vector<vector<int>>(n, vector<int>(m, -1));
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
@@ -70,12 +52,20 @@ int main() {
         }
     }
 
-    if (check()) {
-        t = 0;
-    }
-    else {
-        bfs();
+    bfs();
+
+    int maxa = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (box[i][j] == -1) continue;
+            if (ans[i][j] == -1) {
+                cout << -1 << '\n';
+                return 0;
+            } else {
+                maxa = max(maxa, ans[i][j]);
+            }
+        }
     }
 
-    cout << t << '\n';
+    cout << maxa << '\n';
 }
