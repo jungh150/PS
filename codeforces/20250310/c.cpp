e81ec8b1ac2b0a3249610fadf338ba0537532b87
Junghwa <jungh150@naver.com>
2025-03-11 09:39:08 +0900

    20250310 (2)

---
+++ codeforces/20250310/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        int bn = 2 * n;
+
+        vector<int> b(bn);
+        for (int i = 0; i < bn; i++) cin >> b[i];
+    }
+}
