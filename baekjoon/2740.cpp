06d0875ca6032bf5ce5d30d78207179505bea92d
Junghwa <jungh150@naver.com>
2025-03-22 23:02:08 +0900

    20250322

--- baekjoon/2740.cpp
+++ baekjoon/2740.cpp
@@ -21,10 +21,25 @@ int main() {
 
     vector<vector<int>> b(m, vector<int>(k));
     for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < k; j++) {
             cin >> b[i][j];
         }
     }
 
-    
+    vector<vector<int>> c(n, vector<int>(k, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            for (int l = 0; l < m; l++) {
+                c[i][j] += a[i][l] * b[l][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            cout << c[i][j] << ' ';
+        }
+        cout << '\n';
+    }
 }
