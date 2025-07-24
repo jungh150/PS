1837ca30d82169becbb400c76d2fa94622fe045e
jungh150 <jungh150@naver.com>
2025-07-25 01:12:02 +0900

    20250724

---
+++ baekjoon/11657.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long INTMAX = 1e12;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> edge;
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge.push_back({a, b, c});
+    }
+
+    vector<long long> dst(n + 1, INTMAX);
+    dst[1] = 0;
+
+    bool nc = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int a = edge[j][0];
+            int b = edge[j][1];
+            int c = edge[j][2];
+            if (dst[a] == INTMAX) continue;
+            if (dst[a] + c < dst[b]) {
+                dst[b] = dst[a] + c;
+                if (i == n - 1) nc = true;
+            }
+        }
+    }
+
+    if (nc) {
+        cout << -1 << '\n';
+        return 0;
+    }
+
+    for (int i = 2; i < n + 1; i++) {
+        if (dst[i] == INTMAX) cout << -1 << '\n';
+        else cout << dst[i] << '\n';
+    }
+}
--- baekjoon/1504.cpp
+++ baekjoon/1504.cpp
@@ -3,27 +3,25 @@
 #include <queue>
 using namespace std;
 
-int INTMAX = 10000000;
-int n, e, v1, v2;
-vector<vector<pair<int, int>>> edge;
+int INTMAX = 1e9;
+int n, e;
+vector<vector<pair<int, int>>> adj;
 
-int dijkstra(int s, int e) {
+long long dij(int s, int e) {
     vector<int> dst(n + 1, INTMAX);
-    priority_queue<pair<int, int>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
 
     dst[s] = 0;
-    pq.emplace(0, s);
+    q.emplace(dst[s], s);
 
-    while (!pq.empty()) {
-        int cost = -pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-
-        for (auto nxt: edge[cur]) {
-            int newcost = cost + nxt.second;
-            if (newcost < dst[nxt.first]) {
-                dst[nxt.first] = newcost;
-                pq.emplace(-newcost, nxt.first);
+    while (!q.empty()) {
+        auto [curd, cur] = q.top();
+        q.pop();
+        for (auto [nxt, cost]: adj[cur]) {
+            int newcost = curd + cost;
+            if (newcost < dst[nxt]) {
+                dst[nxt] = newcost;
+                q.emplace(dst[nxt], nxt);
             }
         }
     }
@@ -37,19 +35,20 @@ int main() {
     cout.tie(0);
 
     cin >> n >> e;
-    edge = vector<vector<pair<int, int>>>(n + 1);
+    adj = vector<vector<pair<int, int>>>(n + 1);
 
     for (int i = 0; i < e; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        edge[a].emplace_back(b, c);
-        edge[b].emplace_back(a, c);
+        adj[a].emplace_back(b, c);
+        adj[b].emplace_back(a, c);
     }
 
+    int v1, v2;
     cin >> v1 >> v2;
 
-    int ans1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n);
-    int ans2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n);
+    long long ans1 = dij(1, v1) + dij(v1, v2) + dij(v2, n);
+    long long ans2 = dij(1, v2) + dij(v2, v1) + dij(v1, n);
     if (ans1 >= INTMAX && ans2 >= INTMAX) cout << -1 << '\n';
     else cout << min(ans1, ans2) << '\n';
 }
