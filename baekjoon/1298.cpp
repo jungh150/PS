6a9bf38dc63950407821df971ab0d5249f2668d2
Junghwa <jungh150@naver.com>
2025-11-02 15:09:03 +0900

    20251102

---
+++ baekjoon/1298.cpp
@@ -0,0 +1,79 @@
+// Reference: green55 teamnote
+// https://github.com/green5555/Teamnote/blob/master/TeamNote/BiMatch.cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+const int MAX = 201;
+struct BiMatching {
+    vector<int> adj[MAX+5];
+    int iter, A[MAX+5], B[MAX+5], was[MAX+5];
+
+    bool dfs(int u) {
+        was[u] = iter;
+        for (int v : adj[u]) {
+            if (B[v] == -1) {
+                A[u] = v;
+                B[v] = u;
+                return true;
+            }
+        }
+        for (int v : adj[u]) {
+            if (was[B[v]] != iter && dfs(B[v])) {
+                A[u] = v;
+                B[v] = u;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int biMatch(int n=MAX) {
+        fill(A, A+n, -1);
+        fill(B, B+n, -1);
+        fill(was, was+n, 0);
+        iter = 0;
+        int res = 0;
+        while (true) {
+            iter++;
+            int add = 0;
+            for (int i = 0; i < n; i++) {
+                if (A[i] == -1 && dfs(i)) {
+                    add++;
+                }
+            }
+            if (add == 0) {
+                break;
+            }
+            res += add;
+        }
+        return res;
+    }
+};
+
+void solve() {
+    BiMatching bm;
+
+    cin >> n >> m;
+
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        bm.adj[a].push_back(b + 100);
+    }
+
+    cout << bm.biMatch() << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int T = 1;
+    for (int i = 0; i < T; i++) {
+        solve();
+    }
+}
---
+++ baekjoon/13263.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+
+struct Line {
+    ll a, b;
+    double x;
+    Line(ll a_, ll b_, double x_ = 0) : a(a_), b(b_), x(x_) {}
+};
+
+int n;
+vector<int> a, b;
+vector<ll> dp;
+vector<Line> line;
+
+double cross(Line &f, Line &g) {
+    return (double) (g.b - f.b) / (f.a - g.a);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    // a[i] = 나무의 높이
+    a = vector<int>(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    // b[i] = 나무에 대한 충전 비용
+    b = vector<int>(n);
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    // dp[i] = i번째 나무의 높이를 1로 만들기 위한 최소 비용
+    dp = vector<ll>(n, 0);
+
+    for (int i = 1; i < n; i++) {
+        Line f(b[i - 1], dp[i - 1]); // 새로운 line
+
+        while (line.size() >= 1) {
+            Line g = line.back(); // 가장 최근 line
+            f.x = cross(f, g);
+            if (f.x < g.x) line.pop_back();
+            else break;
+        }
+
+        line.push_back(f); // 새로운 line 삽입
+
+        int l = 0;
+        int r = line.size();
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (line[m].x < a[i]) l = m + 1;
+            else r = m;
+        }
+
+        dp[i] = line[l - 1].a * a[i] + line[l - 1].b;
+    }
+
+    cout << dp[n - 1] << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    for (int i = 0; i < T; i++) {
+        solve();
+    }
+}
