2eed500f9de74edeb3da825de52f6c9eb7376f24
Junghwa <jungh150@naver.com>
2025-12-15 06:36:21 +0900

    20251214

---
+++ baekjoon/3036.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    
+    for (int i = 0; i < n - 1; i++) {
+        int y;
+        cin >> y;
+        int g = __gcd(x, y);
+        cout << x / g << "/" << y / g << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    for (int i = 0; i < T; i++) {
+        solve();
+    }
+}
