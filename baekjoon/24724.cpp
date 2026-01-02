9f67f66c770df3b05aecb72165de9cef115e0f29
Junghwa <jungh150@naver.com>
2026-01-03 04:19:24 +0900

    20260102 (2)

---
+++ baekjoon/24724.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void solve() {
+    int t;
+    cin >> t;
+
+    for (int i = 1; i < t + 1; i++) {
+        cout << "Material Management " << i << '\n';
+        cout << "Classification ---- End!\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    for (int i = 0; i < T; i++) {
+        solve();
+    }
+}
