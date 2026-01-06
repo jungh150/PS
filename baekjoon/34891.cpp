88dd2f529cf55aedb93a5bd07a04ef86a7ba5c6a
Junghwa <jungh150@naver.com>
2026-01-07 01:19:40 +0900

    20260106

---
+++ baekjoon/34891.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n + m - 1) / m << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    for (int i = 0; i < T; i++) {
+        solve();
+    }
+}
