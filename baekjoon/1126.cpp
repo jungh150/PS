d0b336abdecc93e9bf2009262a2444d99687fb2f
jungh150 <jungh150@naver.com>
2026-01-23 09:24:00 +0900

    20260122 (2)

--- baekjoon/1126.cpp
+++ baekjoon/1126.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int MAX_VAL = 5 * 1e5;
+int MIN_VAL = -2 * 1e9;
 
 void solve() {
     int n;
@@ -11,16 +12,20 @@ void solve() {
     vector<int> a(n + 1);
     for (int i = 1; i < n + 1; i++) cin >> a[i];
 
-    vector<vector<int>> dp(n + 1, vector<int>(MAX_VAL + 1, 0));
+    vector<vector<int>> dp(n + 1, vector<int>(MAX_VAL + 1, MIN_VAL));
+    dp[0][0] = 0;
+
     for (int i = 1; i < n + 1; i++) {
         for (int j = 0; j < MAX_VAL + 1; j++) {
             dp[i][j] = dp[i - 1][j];
-            if (j + a[i] < MAX_VAL + 1) dp[i][j] = max(dp[i][j], dp[i - 1][j + a[i]] + a[i]); // 낮은 탑에 올리기
-            dp[i][j] = max(dp[i][j], dp[i - 1][abs(j - a[i])]); // 높은 탑에 올리기
+            if (j + a[i] < MAX_VAL + 1) dp[i][j] = max(dp[i][j], dp[i - 1][j + a[i]] + a[i]);
+            if (a[i] - j >= 0) dp[i][j] = max(dp[i][j], dp[i - 1][a[i] - j] + a[i] - j);
+            if (j - a[i] >= 0) dp[i][j] = max(dp[i][j], dp[i - 1][j - a[i]]);
         }
     }
 
-    cout << dp[n][0] << '\n';
+    if (dp[n][0] == 0) cout << -1 << '\n';
+    else cout << dp[n][0] << '\n';
 }
 
 int main() {
